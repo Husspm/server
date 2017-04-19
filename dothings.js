4089a951a0ac6b3c952d1931ca1c51ce87c05daa
Patrick Hussey <husspm@gmail.com>
2017-04-18 20:16:45 -0400

    functioning

--- dothings.js
+++ dothings.js
@@ -1,11 +1,23 @@
 var http = require("http");
+var url = require("url");
 
 var PORT = 8080;
 
 var server = http.createServer(serverResponse);
 
 function serverResponse(request, response) {
-    response.end("Connected path hit :" + request.url);
+    var urlParts = url.parse(request.url);
+    switch (urlParts.pathname) {
+        case "/":
+        case "/home":
+            response.end("<h1>Connected path hit <a href='/portfolio'>Go to portfolio</a></h1>");
+            break;
+        case "/portfolio":
+            response.end("<h1>Connected path hit <a href='/home'>Go to home</a></h1>");
+            break;
+        default:
+            response.end("Connected path hit :" + request.url);
+    }
 }
 
 
--- server.js
+++ server.js
@@ -7,17 +7,34 @@ var serverA = http.createServer(serverResponseA);
 var serverB = http.createServer(serverResponseB);
 
 function serverResponseA(request, response) {
-    response.end("You made it, I'm glad :" + request.url);
+    response.end(goodResponse[RDM(goodResponse.length)] + request.url);
 }
 
 function serverResponseB(request, response) {
-    response.end("UGGGHHH, you again :" + request.url);
+    response.end(badResponse[RDM(badResponse.length)] + request.url);
 }
 
 serverA.listen(PORTa, function() {
-    console.log("Server has been started. Listening on http://localhost:" + PORTa);
+    console.log("Server has been started. Listening on http://localhost:%s" + PORTa);
 });
 
 serverB.listen(PORTb, function() {
-    console.log("Server has been started. Listening on http://localhost:" + PORTb);
+    console.log("Server has been started. Listening on http://localhost:%s" + PORTb);
 });
+
+function RDM(max) {
+    var newNumber = Math.floor(Math.random() * max);
+    return newNumber;
+}
+var goodResponse = [
+    "Hello there, nice to see you ",
+    "Good afternoon, hows it going ",
+    "Welcome back, good to see you ",
+    "Nice to see you agian "
+];
+var badResponse = [
+    "Oh great its you ",
+    "UGGGGGGHHH, why ",
+    "NONONNNONONONNON ",
+    "FINE, I'll help "
+];
